ae7d591ba9b327e65336af314804941180129fb6
Mamushie <wawerumushie@gmail.com>
2021-10-06 03:01:53 +0300

    Update files

--- _writechar.c
+++ _writechar.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <unistd.h>
-
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -9,6 +8,7 @@
  * Description: _putchar uses a local buffer of 1024 to call write
  * as little as possible
  */
+
 int _putchar(char c)
 {
 	static char buf[1024];
@@ -32,6 +32,7 @@ int _putchar(char c)
  * @str: pointer to the string to print
  * Return: number of chars written
  */
+
 int _puts(char *str)
 {
 	register int i;
